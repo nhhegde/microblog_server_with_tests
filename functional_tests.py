91b0fdd94249cf051591b86e18003867470e4972
nhhegde <nhhegde@users.noreply.github.com>
2017-10-11 15:03:21 -0700

    feat: Added first complete functional test using assertion syntax

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,30 @@
 from selenium import webdriver
 
 browser = webdriver.Firefox()
+
+# Barry has heard about a cool new online to-do app. She goes to 
+# Check out its homepage.
+
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+# She notices the page title and header mention "to-do lists"
+assert 'To-Do' in browser.title, 'Browser Title does not include "To-Do", it was: ' + browser.title
+
+# She is invited to enter a to-do item staright away
+
+# She types "Kidnap a peacock and discover its favorite food" into a text box (Barry's hobby is delighting animals)
+
+# When she hits enter, the page updates, and now the page lists
+# "1: Kidnap a peacock and disvoer its favorite food"
+
+# There is still a text box inviting her to add another item. She
+# enters "Use peacock to fly to the moon"
+
+# The page updates again, and now shows both items on her list.
+
+# Barry wonders whether the site will remember her list -- Then she sees
+# that the site has generated a unique URL for her -- there is some explanatory text to that effect.  
+
+# Satisfied, she goes back to sleep, dreaming of the fun times she will have with a new friend. 
+
+browser.quit()
